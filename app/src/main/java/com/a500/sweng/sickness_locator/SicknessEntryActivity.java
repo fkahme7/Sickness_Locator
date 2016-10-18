76b60bf760816b455ad02d6249aee874d34cf3ae
Ron Baasland <ron.baasland@gmail.com>
2016-10-17 21:08:12 -0400

    Updated/removedold menu files. Refactored some names to be consistent.

---
+++ app/src/main/java/com/a500/sweng/sickness_locator/SicknessEntryActivity.java
@@ -0,0 +1,42 @@
+package com.a500.sweng.sickness_locator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class SicknessEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sickness__entry);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_options_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_entry:
+                startActivity(new Intent(this, SicknessEntryActivity.class));
+                return true;
+            case R.id.menu_map:
+                startActivity(new Intent(this, MapsActivity.class));
+                return true;
+            case R.id.menu_reports:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            case R.id.menu_settings:
+                startActivity(new Intent(this, UserSettings.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/a500/sweng/sickness_locator/UserSettingsActivity.java
@@ -0,0 +1,56 @@
+package com.a500.sweng.sickness_locator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class UserSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+        TabHost.TabSpec tabSpc = tabHost.newTabSpec("user profile");
+        tabSpc.setContent(R.id.userProfileTab);
+        tabSpc.setIndicator("User Profile");
+        tabHost.addTab(tabSpc);
+
+        tabSpc = tabHost.newTabSpec("password");
+        tabSpc.setContent(R.id.passwordTab);
+        tabSpc.setIndicator("Password");
+        tabHost.addTab(tabSpc);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_options_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_entry:
+                startActivity(new Intent(this, SicknessEntryActivity.class));
+                return true;
+            case R.id.menu_map:
+                startActivity(new Intent(this, MapsActivity.class));
+                return true;
+            case R.id.menu_reports:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            case R.id.menu_settings:
+                startActivity(new Intent(this, UserSettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
