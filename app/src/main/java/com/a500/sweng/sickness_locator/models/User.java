f177882ee7f298e34e0573c4262002182ce83a49
Ron Baasland <ron.baasland@gmail.com>
2016-10-26 19:45:38 -0400

    Added dob and gender to users class.

--- app/src/main/java/com/a500/sweng/sickness_locator/models/User.java
+++ app/src/main/java/com/a500/sweng/sickness_locator/models/User.java
@@ -3,6 +3,8 @@ package com.a500.sweng.sickness_locator.models;
 public class User {
     public String name;
     public String email;
+    public String dob;
+    public String gender;
 
     public User() {}
 
@@ -20,5 +22,21 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
     }
 }
