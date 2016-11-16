7c03ad5b283b245674cb08f9bd8e6daea5ca963c
Fawad <fkahme7@gmail.com>
2016-11-15 23:08:14 -0500

    Added log-out to MapsActivity

--- app/src/main/java/com/a500/sweng/sickness_locator/MapsActivity.java
+++ app/src/main/java/com/a500/sweng/sickness_locator/MapsActivity.java
@@ -27,6 +27,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
